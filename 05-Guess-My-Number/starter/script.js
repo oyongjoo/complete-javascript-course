5afeb9da3afea6a041f2dec5b2a5a2e31a5df754
Yongjoo Lee <oyongjoo@gmail.com>
2021-11-03 23:50:23 +0900

    registry click event and function call in addEventLIstner

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,18 @@
 'use strict';
+
+/* console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = '🎉 Correct Number';
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value); */
+
+const x = function () {
+  console.log(23);
+};
+
+document.querySelector('.check').addEventListener('click', function () {
+  console.log(document.querySelector('.guess').value);
+});
