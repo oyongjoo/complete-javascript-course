653821cae648750f10f5eae9d53038e039ebaf7b
Yongjoo Lee <oyongjoo@gmail.com>
2021-11-05 07:20:45 +0900

    조건식을 통해서 guess 상황에 따라서 message 출력 및 주석 추가

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -9,10 +9,39 @@ document.querySelector('.score').textContent = 10;
 document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value); */
 
-const x = function () {
-  console.log(23);
-};
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
-  console.log(document.querySelector('.guess').value);
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  // When there is no inut
+  if (!guess) {
+    document.querySelector('.message').textContent = '😳 No number!';
+
+    // When player wins
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🎉 Correct Number!';
+
+    // When guess is to high
+  } else if (guess > secretNumber) {
+    if (score > 0) {
+      document.querySelector('.message').textContent = '📈 Too hgih';
+      score--;
+    } else {
+      document.querySelector('.message').textContent = '💥 You lost the game';
+    }
+
+    // When guess is too low
+  } else if (guess < secretNumber) {
+    if (score > 0) {
+      document.querySelector('.message').textContent = '📉 Too low';
+      score--;
+    } else {
+      document.querySelector('.message').textContent = '💥 You lost the game';
+    }
+  }
+  document.querySelector('.score').textContent = score;
 });
