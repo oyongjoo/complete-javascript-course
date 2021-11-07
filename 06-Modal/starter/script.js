9fef751d7d10c036620df02e6e0346e589557221
Yongjoo Lee <oyongjoo@gmail.com>
2021-11-07 09:55:35 +0900

    [Modal] add codes 1. assign const variables with classes 2. Use querySelectorAll to get same class name list 3. classlist.remove to remove hidden class 4. Use modal.classList.add('hidden') to close modal window. 5. add funtion closeModel to clean up code.

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,26 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnOpenModal = document.querySelectorAll('.show-modal');
+const btnCloseModal = document.querySelector('.close-modal');
+
+console.log(btnOpenModal);
+
+// function closes Modal window
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnOpenModal.length; i++) {
+  console.log(btnOpenModal[i]);
+  btnOpenModal[i].addEventListener('click', function () {
+    console.log('Button Clicked');
+    modal.classList.remove('hidden'); // remove hidden attribute from class
+    overlay.classList.remove('hidden');
+  });
+}
+
+btnCloseModal.addEventListener('click', closeModal); // call function
+overlay.addEventListener('click', closeModal);
